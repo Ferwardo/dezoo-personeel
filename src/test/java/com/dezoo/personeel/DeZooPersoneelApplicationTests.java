56ce11b3394d4f9513229ad541b0d8b2ade9512d
Ferwardo <r0741339@student.thomasmore.be>
2020-10-28 15:23:59 +0100

    Initial project

---
+++ src/test/java/com/dezoo/personeel/DeZooPersoneelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dezoo.personeel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeZooPersoneelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
